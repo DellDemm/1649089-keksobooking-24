613eda0f41528b7730ea34500ef85835a53c1a79
DellDemm <81381924+DellDemm@users.noreply.github.com>
2021-10-19 21:08:28 +0300

    Внесены правки в код

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 //Функция возврата целого числа из заданного диапазлна(Включительно)
 function getRandom(min, max) {
   if (max <= min){
-    console.log('Введены не коректные значения')
+    throw('Введены не коректные значения');
   }
   return Math.floor(Math.random()*(max-min))+min;
 }
-getRandom(10,15)
+getRandom(10,15);
 
 
 // функция возврата заданного числа из диапазона
 
-function getRandomFloat(min,max) {
-  if(max <= min){
-    console.log('Введены некорректные значения!')
+const getRandomIntFromToWithComma = (from, to, countSignsAfterComma) => {
+  if (from < 0 || to <= from) {
+    throw('Введены не коректные значения');
   }
-    return Math.random() * (max-min) + min;
-}
-getRandomFloat(10,12)
+  return +(Math.random() * (to - from)).toFixed(countSignsAfterComma) + from;
+};
+
+getRandomIntFromToWithComma(0.4, 4.9, 3);
