2182e0ffdb3b9397ebd15928c9d7a86dd47d17ff
DellDemm <81381924+DellDemm@users.noreply.github.com>
2021-10-26 15:56:17 +0300

    Внес правки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-//Импортирую генераторы рандомных чисел
-import {getRandomPositiveFloat} from './utils/get-random-positive-float';
-import {getRandomPositiveInteger} from './utils/get-random-positive-integer';
-
+//Функция возврата целого числа из заданного диапазлна(Включительно)
+function getRandom(min, max) {
+  if (max <= min){
+    throw('Введены не коректные значения');
+  }
+  return Math.floor(Math.random()*(max-min))+min;
+}
+// функция возврата заданного числа из диапазона
+const getRandomIntFromToWithComma = (from, to, countSignsAfterComma) => {
+  if (from < 0 || to <= from) {
+    throw('Введены не коректные значения');
+  }
+  return +(Math.random() * (to - from)).toFixed(countSignsAfterComma) + from;
+};
 // Добавляю массивы с данными
 const TITLES = [
   'Девичье гнездышко',
@@ -9,13 +19,12 @@ const TITLES = [
   'Карнавал текстур и красок',
   'Продуманная рациональность',
   'Приют всех муз',
-  'Яркое отражение индивидуальности'];
-const PRICE = {min:'1000', max:'50000'};
+  'Яркое отражение индивидуальности',];
+const PRICE = {min:1000, max:10000};
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const ROOMS = {min:'1', max:'5'};
-const GUESTS = {min: '1', max:'5'};
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const ROOMS ={min:1, max:5};
+const GUESTS = {min: 1, max:5};
+const TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const DESCRIPTIONS = [
   'ИДЕАЛЬНЫЙ ВАРИАНТ ДЛЯ ТЕХ, КТО ХОЧЕТ РАЗМЕСТИТЬСЯ В КВАРТИРЕ С НОВЫМ РЕМОНТОМ В ЦЕНТРЕ ГОРОДА',
@@ -30,43 +39,70 @@ const PHOTOS = [
 ];
 const LOCATION = {
   lat:{
-    min: '35.65000',
-    max: '35.70000',
+    min: 35.65000,
+    max: 35.70000,
   },
   lng:{
-    min: '139.70000',
-    max: '139.80000',
+    min: 139.70000,
+    max: 139.80000,
   },
 };
+
+//функция выбора случайных Удобств
+const addFeatures = (featuresLits) => {
+  const features = [];
+  features.push(featuresLits[getRandom(0,5)]);
+  for (let ft = 0; ft < 6; ft++) {
+    const randomValue = getRandom(0, 5);
+    if (features.includes(featuresLits[randomValue])) {
+      continue;
+    }else{
+      features.push(featuresLits[randomValue]);
+    }
+  }
+  return features;
+};
+//Функция выбора случайных фото
+const addPhotos = (photosLits) => {
+  const photos = [];
+  photos.push(photosLits[getRandom(0,2)]);
+  for (let ft = 0; ft < 3; ft++) {
+    const randomValue = getRandom(0, 2);
+    if (photos.includes(photosLits[randomValue])) {
+      continue;
+    }else{
+      photos.push(photosLits[randomValue]);
+    }
+  }
+  return photos;
+};
+
 // функция создания объявления
 const createOffer = () => ({
+  const LAT = getRandomIntFromToWithComma(LOCATION.lat.min, LOCATION.lat.max, 5),
+  const LNG = getRandomIntFromToWithComma(LOCATION.lng.min, LOCATION.lng.max, 5),
   return: {
     author: {
-      avatar:'img/avatars/user{{xx}}.png'
+      avatar:`img/avatars/user${getRandom(1,10)}.png`,
     },
     offer: {
-      title: TITLES[getRandomPositiveInteger(0,TITLES.length - 1)],
-      address: [
-        LOCATION.lat,
-        LOCATION.lng,
-      ],
-      price: getRandomPositiveInteger(PRICE.min, PRICE.max),
-      type: TYPES[getRandomPositiveInteger(0,TYPES.length - 1)],
-      rooms: getRandomPositiveInteger(ROOMS.min, ROOMS.max),
-      quests: getRandomPositiveInteger(GUESTS.min, GUESTS.max),
-      checkin: CHECKIN[getRandomPositiveInteger(0, CHECKIN.length - 1)],
-      checkout: CHECKOUT[getRandomPositiveInteger(0,CHECKOUT.length - 1)],
-      features: FEATURES[getRandomPositiveInteger(0, FEATURES.length - 1)],
-      descroption: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
-      photos: PHOTOS[getRandomPositiveInteger(0, PHOTOS.length - 1)],
+      title: TITLES[getRandom(0,TITLES.length - 1)],
+      address: `${LAT},${LNG}`,
+      price: getRandom(PRICE.min, PRICE.max),
+      type: TYPES[getRandom(0,TYPES.length - 1)],
+      rooms: getRandom(ROOMS.min, ROOMS.max),
+      quests: getRandom(GUESTS.min, GUESTS.max),
+      checkin: TIMES[getRandom(0, TIMES.length - 1)],
+      checkout: TIMES[getRandom(0, TIMES.length - 1)],
+      features: addFeatures(FEATURES),
+      description: DESCRIPTIONS[getRandom(0,DESCRIPTIONS.length-1)],
+      photos: addPhotos(PHOTOS),
     },
     location: {
-      lat: getRandomPositiveFloat(LOCATION.lat.min, LOCATION.lat.max, 5),
-      lng: getRandomPositiveFloat(LOCATION.lng.min, LOCATION.lng.max, 5),
+      lat: LAT,
+      lng: LNG,
     },
   },
 });
-const ADSOFFERS = [];
-for(let count = 0; count < 10; count++){
-  ADDSOFFERS[count] = createOffer[count];
-};
+const adoffer = Array.from({ length: 10 }, createOffer);
+throw(adoffer);
